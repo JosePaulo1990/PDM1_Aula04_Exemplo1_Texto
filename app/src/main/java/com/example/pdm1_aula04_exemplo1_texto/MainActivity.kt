2f20b14994c1753f19b8cee23f90735cfcc50c7a
JosePaulo1990 <josepaulo.silvalima@gmail.com>
2024-10-25 16:25:20 -0300

    Versão 1.0

--- app/src/main/java/com/example/pdm1_aula04_exemplo1_texto/MainActivity.kt
+++ app/src/main/java/com/example/pdm1_aula04_exemplo1_texto/MainActivity.kt
@@ -2,46 +2,20 @@ package com.example.pdm1_aula04_exemplo1_texto
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pdm1_aula04_exemplo1_texto.ui.theme.PDM1_Aula04_Exemplo1_TextoTheme
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PDM1_Aula04_Exemplo1_TextoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        // Referenciando o TextView e o Button
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener { // Definindo o evento de clique do botão
+            textView.text = "Prof. José Paulo" // Alterando o texto do TextView
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PDM1_Aula04_Exemplo1_TextoTheme {
-        Greeting("Android")
     }
 }
